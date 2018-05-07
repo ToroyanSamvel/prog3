5e9ca157e889ba25c340ebff6b060f0221666150
Your Name <you@example.com>
2018-05-07 19:08:47 +0400

    node

---
+++ lesson3/index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.send("Hello world");
+});
+
+app.get("/name", function(req, res){
+   res.send("Hello world name");
+});
+
+app.get("/name/:name", function(req, res){
+    var n = req.params.name;
+   res.send("<h1>Hello" + n + "</h1>");
+});
+
+app.get("/search", function(req, res){
+   res.redirect("https://www.google.am/");
+});
+
+app.get("/*", function(req, res){
+   res.send("404 not found");
+});
+
+
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
